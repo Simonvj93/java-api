971665ffc35f7994335d3a71c951b73d3dab40bd
Simon Soiland <simon@funbit.no>
2022-12-03 22:10:59 +0100

    created entity, repo, controller and autoloader for db data. API is working

--- javaapi/src/main/java/com/example/javaapi/JavaapiApplication.java
+++ javaapi/src/main/java/com/example/javaapi/JavaapiApplication.java
@@ -1,4 +1,4 @@
-package com.javaapi.javaapi;
+package com.example.javaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ javaapi/src/main/java/com/example/javaapi/WatchRepository.java
@@ -0,0 +1,7 @@
+package com.example.javaapi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface WatchRepository extends JpaRepository<Watch, Long> {
+
+}
